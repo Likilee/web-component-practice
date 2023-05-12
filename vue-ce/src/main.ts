aed2ded37e6fc9c351359f65c8d1cbd4832bf174
hyunja27 <123gk@naver.com>
2023-05-12 16:50:49 +0900

    05/12_hyunja_webComponent VS vue

--- vue-ce/src/main.ts
+++ vue-ce/src/main.ts
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import CustomCounter from "./web-components/custom-counter.js";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+customElements.define("custom-counter", CustomCounter);
+createApp(App).mount("#app");
---
+++ vue-ce/src/web-components/custom-counter.js
@@ -0,0 +1,53 @@
+export default class CustomCounter extends HTMLElement {
+  count;
+
+  constructor() {
+    super();
+
+    this.count = this.getAttribute("count") ?? 0;
+    const shadow = this.attachShadow({ mode: "closed" });
+
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("id", "wrapper");
+
+    const divElement = document.createElement("div");
+
+    const pTag = document.createElement("p");
+    pTag.innerText = "counter: ";
+
+    const countElement = document.createElement("p");
+    countElement.setAttribute("id", "count");
+    countElement.innerText = this.count;
+
+    const upButton = document.createElement("button");
+    upButton.innerText = "up";
+    upButton.setAttribute("id", "up-button");
+    upButton.addEventListener("click", this.handleIncrease.bind(this));
+
+    const downButton = document.createElement("button");
+    downButton.innerText = "down";
+    downButton.setAttribute("id", "down-button");
+    downButton.addEventListener("click", this.handleDecrease.bind(this));
+
+    divElement.appendChild(pTag);
+    divElement.appendChild(countElement);
+    wrapper.appendChild(divElement);
+    wrapper.appendChild(upButton);
+    wrapper.appendChild(downButton);
+
+    shadow.appendChild(wrapper);
+  }
+
+  handleIncrease(e) {
+    this.count++;
+    const countElement = this.shadowRoot.getElementById("count");
+    countElement.innerText = this.count;
+  }
+  handleDecrease() {
+    this.count--;
+    const countElement = this.shadowRoot.getElementById("count");
+    countElement.innerText = this.count;
+  }
+}
+
+// customElements.define("custom-counter", CustomCounter);
